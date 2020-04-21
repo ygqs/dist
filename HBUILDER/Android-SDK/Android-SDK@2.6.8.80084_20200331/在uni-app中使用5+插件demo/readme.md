8022890855442b90e9bfd92f5bdbc907ffc1d54c
安厦 <15147906690@163.com>
2020-04-21 16:56:20 +0800

    asd
    asd

---
+++ HBUILDER/Android-SDK/Android-SDK@2.6.8.80084_20200331/在uni-app中使用5+插件demo/readme.md
@@ -0,0 +1,9 @@
+## 使用方法：
+1. 本示例运行前需通过[HBuilderX](http://www.dcloud.io/hbuilderx.html)进行编译才能正常运行。
+2. 参考[Android平台，uni-app离线打包集成要领及注意事项](http://ask.dcloud.net.cn/article/35139)，确保正常运行。
+3. 插件编写规则按照[Android第三方插件开发指导](http://ask.dcloud.net.cn/article/66)进行开发。
+4. plugins.js 已做5+兼容。
+
+## 注意：
+1. 当前demo对应的android层即H5PlusPlugin/PGPlugintest.java文件，使用时只需编译当前demo，切换dcloud_control.xml中的appid即可。
+2. 白屏问题参考[Android平台，uni-app离线打包集成要领及注意事项](http://ask.dcloud.net.cn/article/35139)
---
+++ HBUILDER/Android-SDK/Android-SDK@2.6.8.80084_20200331/在uni-app中使用5+插件demo/uni-plugin-demo/pages/index/index.vue
@@ -0,0 +1,66 @@
+<template>
+    <view>
+        <button @click="pluginShow">PluginTestFunction</button>
+		<button @click="pluginShowArrayArgu">PluginTestFunctionArrayArgu</button>
+		<button @click="pluginGetString">PluginTestFunctionSync</button>
+		<button @click="pluginGetStringArrayArgu">PluginTestFunctionSyncArrayArgu</button>
+    </view>
+</template>
+<style>
+	button {
+		width: 94%;
+		margin: 20upx auto;
+	}
+</style>
+<script>
+// 扩展的 js 文件的位置：common/plugins.js
+var plugins = require('../../common/plugins.js');
+export default {
+    data() {
+        return {
+            plugins: plugins
+        };
+    },
+    methods: {
+        pluginShow() {
+            this.plugins.PluginTestFunction(
+                'Html5',
+                'Plus',
+                'AsyncFunction',
+                'MultiArgument!',
+                function(result) {
+					uni.showToast({title:JSON.stringify(result),icon:'none'});
+                },
+                function(result) {
+					uni.showToast({title:result});
+                }
+            );
+        },
+        pluginShowArrayArgu() {
+            this.plugins.PluginTestFunctionArrayArgu(
+                ['Html5', 'Plus', 'AsyncFunction', 'ArrayArgument!'],
+                function(result) {
+                    uni.showToast({title:result,icon:'none'});
+                },
+                function(result) {
+                    uni.showToast({title:result,icon:'none'});
+                }
+            );
+        },
+        pluginGetString() {
+			uni.showToast({title:this.plugins.PluginTestFunctionSync('Html5', 'Plus', 'SyncFunction', 'MultiArgument!'),icon:'none'});
+        },
+        pluginGetStringArrayArgu() {
+            var Argus = this.plugins.PluginTestFunctionSyncArrayArgu([
+                'Html5',
+                'Plus',
+                'SyncFunction',
+                'ArrayArgument!'
+            ]);
+			uni.showToast({title:Argus.RetArgu1 + '_' + Argus.RetArgu2 + '_' + Argus.RetArgu3 + '_' + Argus.RetArgu4,icon:'none'});
+        }
+    }
+};
+</script>
+<style>
+</style>
